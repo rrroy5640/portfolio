ac234684da0f498c01c78070a6aabe00b6d647bc
rrroy <hly5640@gmail.com>
2024-03-16 23:04:03 +1100

    change: bug found, project description div cannot be absolute

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import projects from "../projects";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -21,24 +20,21 @@ export const ProjectCard = () => {
   return (
     <div className=" p-14 max-w-sm md:max-w-md lg:max-w-md mx-auto items-center">
       <Slider {...settings}>
-        {
-            projects.map((project, index) => {
-                return (
-                    <div key={index} className="w-full">
-                        <img src={project.image} alt={project.title} className="rounded-lg w-full h-96 object-cover" />
-                        <div className="px-4 py-2">
-                            <h1 className="text-3xl font-bold text-violet-400">{project.title}</h1>
-                            <p className="text-lg text-violet-400">{project.description}</p>
-                            <a href={project.url} target="_blank" rel="noreferrer">
-                                <button className="mt-2 text-white bg-indigo-600 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-700 rounded text-lg">
-                                    View Project
-                                </button>
-                            </a>
-                        </div>
-                    </div>
-                );
-            })
-        }
+        {projects.map((project, index) => {
+          return (
+            <div key={index} className="w-full group relative">
+              <a href={project.url} target="_blank" rel="noreferrer">
+                <img
+                  src={project.image}
+                  className="rounded-lg w-full h-full object-cover"
+                />
+              </a>
+              <div className="px-4 py-2 bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500 ">
+                <p className="text-lg text-white">{project.description}</p>
+              </div>
+            </div>
+          );
+        })}
       </Slider>
     </div>
   );
