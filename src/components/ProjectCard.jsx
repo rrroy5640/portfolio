3d675bf2218de4c4f6d50c572d72fb90418ccdbb
rrroy <hly5640@gmail.com>
2024-03-16 21:27:30 +1100

    change: apply react-slick as project corousel

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,46 +1,41 @@
 import { useState } from "react";
 import projects from "../projects";
-import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export const ProjectCard = () => {
-  const [current, setCurrent] = useState(0);
-
-  const nextProject = () => {
-    setCurrent((current + 1) % projects.length);
-  };
-
-  const prevProject = () => {
-    setCurrent((current - 1 + projects.length) % projects.length);
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    swipeToSlide: true,
   };
 
   return (
-    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl relative group">
-      <button
-        onClick={prevProject}
-        aria-label="Previous project"
-        className="absolute left-1 top-1/2 transform -translate-y-1/2 z-20 m-2 bg-black bg-opacity-50 rounded-full p-2 hover:bg-opacity-70 transition-opacity duration-300"
-      >
-        <IoIosArrowBack size="24px" className="text-white" />
-      </button>
-      <a href={projects[current].url} target="_blank" rel="noopener noreferrer">
-        <img
-          src={projects[current].image}
-          alt={`Project ${current + 1}`}
-          className="w-64 md:w-72 md:min-w-72"
-        />
-        <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500">
-          <p className="text-white text-lg p-4">
-            {projects[current].description}
-          </p>
-        </div>
-      </a>
-      <button
-        onClick={nextProject}
-        aria-label="Next project"
-        className="absolute right-1 top-1/2 transform -translate-y-1/2 z-20 m-2 bg-black bg-opacity-50 rounded-full p-2 hover:bg-opacity-70 transition-opacity duration-300"
-      >
-        <IoIosArrowForward size="24px" className="text-white" />
-      </button>
+    <div className=" p-14 max-w-sm md:max-w-md lg:max-w-md mx-auto items-center">
+      <Slider {...settings}>
+        {projects.map((project, index) => (
+          <div key={index} className=" items-center  group relative">
+            <a href={project.url} target="_blank" rel="noopener noreferrer">
+              <img
+                src={project.image}
+                alt={`Project ${index}`}
+                className="w-full"
+              />
+              <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+                <p className="text-white text-lg p-4">{project.description}</p>
+              </div>
+            </a>
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
