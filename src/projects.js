80bdae6640cef57ba67f69f91a27aa69a0cf058d
rrroy <hly5640@gmail.com>
2024-04-02 17:58:05 +1100

    add card project

--- src/projects.js
+++ src/projects.js
@@ -1,14 +1,21 @@
 const projects = [
-    {
-      image: '/DevConnect.png',
-      url: 'https://infinite-garden-00475-a26120a06418.herokuapp.com',
-      description: 'MERN stack (with redux toolkit) social network for developers'
-    },
-    {
-      image:'/musicapp.png',
-      url: 'https://github.com/rrroy5640/MusicAppUI',
-      description: 'Android music app UI with Jetpack Compose'
-    }
-  ];
+  {
+    image: "news-card.png",
+    url: "https://news-card.linyi-dev.com/",
+    description:
+      "Get latest news anytime, implemented with React and Express, styled with Tailwind CSS",
+  },
+  {
+    image: "/DevConnect.png",
+    url: "https://infinite-garden-00475-a26120a06418.herokuapp.com",
+    description:
+      "MERN stack (with redux toolkit) social network for developers",
+  },
+  {
+    image: "/musicapp.png",
+    url: "https://github.com/rrroy5640/MusicAppUI",
+    description: "Android music app UI with Jetpack Compose",
+  }
+];
 
 export default projects;
