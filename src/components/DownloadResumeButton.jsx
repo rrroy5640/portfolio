6910b0968b9cec2faa5fc65b64b7fa5ed3a4ab21
rrroy <hly5640@gmail.com>
2024-09-27 09:58:09 +1000

    updated resume

--- src/components/DownloadResumeButton.jsx
+++ src/components/DownloadResumeButton.jsx
@@ -1,11 +1,11 @@
 export const DownloadResumeButton = () => {
-    return (
-      <a
-        href="/Resume.pdf" 
-        download="Resume Linyi Huang.pdf"
-        className=" bg-indigo-600 hover:bg-indigo-500 w-44 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out"
-      >
-        Download Resume
-      </a>
-    );
-  };
+  return (
+    <a
+      href="/Linyi (Roy) Huang - Developer.docx"
+      download="Linyi (Roy) Huang - Developer.docx"
+      className=" bg-indigo-600 hover:bg-indigo-500 w-44 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out"
+    >
+      Download Resume
+    </a>
+  );
+};
