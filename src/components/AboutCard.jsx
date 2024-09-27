7278fe0a749d65bb3402d5bb1a8f5ae3de761ce0
rrroy <hly5640@gmail.com>
2024-09-27 15:13:56 +1000

    UPDATE: font size

--- src/components/AboutCard.jsx
+++ src/components/AboutCard.jsx
@@ -11,10 +11,15 @@ export const AboutCard = () => {
   const details = () => {
     if (selectedTab === "skills") {
       return (
-        <div className="h-48 w-64 md:w-72 lg:w-96 overflow-auto text-xl text-gray-300">
+        <div className="h-48 w-64 md:w-72 lg:w-96 overflow-auto text-base text-gray-300">
           <ul className="list-disc list-inside">
-            <li>HTML, CSS, Javascript, TypeScript, React, Next.js, Redux, TailwindCSS</li>
-            <li>C#, ASP.NET Core, Express, Node.js, MongoDB, SQL, RESTful API</li>
+            <li>
+              HTML, CSS, Javascript, TypeScript, React, Next.js, Redux,
+              TailwindCSS
+            </li>
+            <li>
+              C#, ASP.NET Core, Express, Node.js, MongoDB, SQL, RESTful API
+            </li>
             <li>AWS, Docker, Git, Jenkins, ECS, Fargate, CI/CD</li>
           </ul>
         </div>
@@ -22,7 +27,7 @@ export const AboutCard = () => {
     }
     if (selectedTab === "education") {
       return (
-        <div className="h-48 w-64 md:w-72 lg:w-96 overflow-auto text-xl text-gray-300">
+        <div className="h-48 w-64 md:w-72 lg:w-96 overflow-auto text-base text-gray-300">
           <ul className="list-disc list-inside">
             <li>
               <span className="font-bold">University of Tasmania</span>
@@ -35,7 +40,7 @@ export const AboutCard = () => {
     }
     if (selectedTab === "certification") {
       return (
-        <div className="h-48 w-64 md:w-72 lg:w-96 overflow-auto text-xl text-gray-300">
+        <div className="h-48 w-64 md:w-72 lg:w-96 overflow-auto text-base text-gray-300">
           <ul className="list-disc list-inside">
             <li>AWS Solutions Architect Associate</li>
           </ul>
@@ -51,19 +56,19 @@ export const AboutCard = () => {
           onClick={() => handleTabClick("skills")}
           className={`mr-4 pb-2 ${selectedTab === "skills" ? " text-violet-500 border-b-2 border-violet-500 font-medium" : "text-slate-300"}`}
         >
-          <h1 className="text-2xl font-bold">Skills</h1>
+          <h1 className="text-xl font-bold">Skills</h1>
         </button>
         <button
           onClick={() => handleTabClick("education")}
           className={`mr-4 pb-2 ${selectedTab === "education" ? " text-violet-500 border-b-2 border-violet-500 font-medium" : " text-slate-300"}`}
         >
-          <h1 className="text-2xl font-bold">Education</h1>
+          <h1 className="text-xl font-bold">Education</h1>
         </button>
         <button
           onClick={() => handleTabClick("certification")}
           className={`mr-4 pb-2 ${selectedTab === "certification" ? " text-violet-500 border-b-2 border-violet-500 font-medium" : " text-slate-300"}`}
         >
-          <h1 className="text-2xl font-bold">Certification</h1>
+          <h1 className="text-xl font-bold">Certification</h1>
         </button>
       </div>
       <div>{details()}</div>
